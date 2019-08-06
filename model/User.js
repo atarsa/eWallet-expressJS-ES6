9a675e02671fd63b4f9d64fb1c08918d378e9f23
ATarsa <smb909@gmail.com>
2019-08-06 16:46:53 +0100

    Add wallet to user schema

--- model/User.js
+++ model/User.js
@@ -1,5 +1,42 @@
 const mongoose = require('mongoose');
 
+
+// List of available currencies
+/* list compatible with https://exchangeratesapi.io/ and foreign exchange rates published by the European Central Bank https://www.ecb.europa.eu/stats/policy_and_exchange_rates/euro_reference_exchange_rates/html/index.en.html */
+const availableCurrencies = ['EUR', 'USD', 'JPY','BGN','CZK',
+'DKK','GBP','HUF' ,'PLN' ,'RON' ,'SEK' ,'CHF' ,'ISK' ,'NOK' ,'HRK' ,'RUB' ,'TRY' ,'AUD' ,'BRL','CAD','CNY','HKD' ,'IDR','ILS' ,'INR','KRW','MXN','MYR','NZD','PHP','SGD','THB','ZAR'];
+
+
+// Create wallet schema
+const WalletSchema = new mongoose.Schema({
+  baseCurrency: {
+    type: String,
+    min: 3,
+    max: 3,
+    default: 'GBP',
+    uppercase: true
+  },
+  items: {
+    id: {
+      type: Number,
+      min: 0
+    },
+    currency: {
+      type: String,
+      min: 3,
+      max: 3,
+      uppercase: true,
+      enum: availableCurrencies
+    },
+    amount: {
+      type: Number,
+      min: 0
+    }
+  }
+
+});
+
+// Create user schema
 const userSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -14,10 +51,8 @@ const userSchema = new mongoose.Schema({
     min: 6,
     max: 1024
   },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+  // add wallet
+  wallet: WalletSchema
 });
 
 module.exports = mongoose.model('User', userSchema);
