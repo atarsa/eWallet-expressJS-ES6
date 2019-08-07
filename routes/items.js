5956d429c3435b9106d6106f5879a3808342622f
ATarsa <smb909@gmail.com>
2019-08-07 18:39:49 +0100

    Update PUT and DELEte routes for items

--- routes/items.js
+++ routes/items.js
@@ -7,7 +7,7 @@ router.get('/', verify, (req, res) => {
   
   Wallet.findById(req.wallet._id).then( (wallet) => {
     
-    res.json(wallet);
+    res.json(wallet.items);
   });
   
   //* here, we added verify middleware, so only logged users can see posts
@@ -23,106 +23,54 @@ router.post('/', verify, (req, res) => {
   };
   console.log('req.wallet._id: ', req.wallet._id);
   // TODO: respone back; 
-  Wallet.findByIdAndUpdate(req.wallet._id, {$push: { items : item}}, {safe: true, new : true}, (err, item) => {
+  Wallet.findByIdAndUpdate(req.wallet._id, {$push: { items : item}}, {safe: true, new : true}, (err, wallet) => {
     
       // Handle any possible database errors
       if (err) {
         console.log(err)
         return res.status(500).send(err)};
-      return res.send(item);
+      return res.send(wallet.items);
     })
-
-  // console.log(req.wallet._id)
-  // Wallet.findById(req.wallet._id, (err, item) => {
-  //   // Handle any possible database errors
-  //   if (err) {
-  //     console.log(err)
-  //     return res.status(500).send(err)};
-  //   return res.send(item);
-  // })
 
 });
 
- // TODO: respone back; 
+ // TODO: response back; 
 // Delete item from wallet
 router.delete('/:id', verify, (req, res) =>{
-  Wallet.findByIdAndUpdate(req.user._id,
-     {$pull: { "wallet.items" : {id: req.params.id}}}, 
-     {safe: true, upsert: true, new : true}, 
-     (err, user) => {
+  Wallet.findByIdAndUpdate(req.wallet._id,
+     {$pull: { "items" : {"id": req.params.id}}}, 
+     {safe: true, new: true}, 
+     (err, wallet) => {
         // Handle any possible database errors
         if (err) {
           console.log(err)
           return res.status(500).send(err)};
         console.log(req.params.id);
-        return res.send(user.wallet);
+        return res.send(wallet.items);
      })
 });
 
- // !NOT WORKING; 
+// *For now PUT method by id, should be changed to currency as suposed to be unique??
 // Update amount of item in wallet
-router.put('/items/:currency', verify, (req, res) =>{
+router.put('/:id', verify, (req, res) =>{
 
-  // db.SCSIssue.find(
-  //   { _id: ObjectId("5439a2992ea8cc0f70feef2d") }, 
-  //   { Statuses: { $elemMatch: { StatusID: { $gte : NumberLong(521055087020736513) } } } }
-  //  )
-  User.updateOne({
-    _id: req.user._id , 
-    "items": {$elemMatch : {currency: req.body.currency}}}
-  ,
-  {$set: { "items.$.amount" : req.body.amount}}
-  
-  ).then( (err, item) => {
-
-    console.log(item);
-    if (err){
-        res.send(err)
-    }    
-
-    if (item) {
-        console.log("Item Found");
-        res.send(item);
-
-    }
-  }) 
-  
+  Wallet.findOneAndUpdate(
+    {
+      _id: req.wallet._id,
+      "items.id": 
+        req.params.id
+    },
+    {$set: { "items.$.amount" : req.body.amount}}, 
+    
+    {safe: true, new : true}, 
+    (err, wallet) => {
+    // Handle any possible database errors
+      if (err) {
+        console.log(err)
+        return res.status(500).send(err)};
       
-
-  
-  // User.update(
-  //   {
-    
-  //   "wallet.items.currency": 
-  //     req.body.currency 
-  // },
-  //   {
-  //     $set: {"wallet.items.$.amount": req.body.amount}
-  //   }
-  // ).then(
-  //   (data) => {
-  //     console.log(data);
-  //   });
-  
-  // User.findOneAndUpdate(
-  //   {
-  //     _id: req.user._id,
-  //     "wallet.items.id": 
-  //       req.params.id
-        
-            
-  //   },
-  //   {$set: { "items.$.amount" : req.body.amount}}, 
-    
-  //   {safe: true, new : true}, 
-  //   (err, user) => {
-  //   // Handle any possible database errors
-  //     if (err) {
-  //       console.log(err)
-  //       return res.status(500).send(err)};
-  //     console.log(user);
-  //     return res.send(user.wallet);
-  //   })
+      return res.send(wallet.items);
+    })
 });
 
 module.exports = router;
